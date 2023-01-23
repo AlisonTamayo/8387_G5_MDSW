4774033e66addc1d2b0543cbe10880174412afa7
DavidSangoquiza <121050747+DavidSangoquiza@users.noreply.github.com>
2023-01-22 20:54:11 -0500

    GH v1.4
    Cambios realizados

---
+++ CODE/src/modelo/usuarios.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+
+public class usuarios {
+    
+    private int userID;
+    private String cedula;
+    private String nombre_apellido;;
+    private String telefono;    
+    private String correo;
+    private String numeroCasa;
+    private String estadoUsuario;
+    private String nombreDueño;
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre_apellido() {
+        return nombre_apellido;
+    }
+
+    public void setNombre_apellido(String nombre_apellido) {
+        this.nombre_apellido = nombre_apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    public void setEstadoUsuario(String estadoUsuario) {
+        this.estadoUsuario = estadoUsuario;
+    }
+
+    public String getNombreDueño() {
+        return nombreDueño;
+    }
+
+    public void setNombreDueño(String nombreDueño) {
+        this.nombreDueño = nombreDueño;
+    }
+    
+    
+
+    
+}
